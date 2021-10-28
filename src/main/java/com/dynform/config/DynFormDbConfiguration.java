3cb28e17d28db71bd3fa73a523050a9a10247cf3
fabio.biffoli <fabio.biffoli@dedalus.eu>
2021-10-28 08:41:43 +0200

    Configurations

--- src/main/java/com/dynform/config/DynFormDbConfiguration.java
+++ src/main/java/com/dynform/config/DynFormDbConfiguration.java
@@ -20,7 +20,7 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 public class DynFormDbConfiguration {
-	
+	/*
 	@Configuration
 	@EnableTransactionManagement
 	@EnableJpaRepositories(
@@ -64,5 +64,5 @@ public class DynFormDbConfiguration {
 			return new JpaTransactionManager(dynFormEntityManagerFactory);
 		}
 	}
-	
+	*/
 }
